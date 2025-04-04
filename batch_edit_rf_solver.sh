59800b2364de5f4c76cc1dd3d0f76ec7b83319e0
HocChueng <1401698558@qq.com>
2025-04-04 23:33:10 +0800

    添加批处理脚本batch_edit_rf_solver.sh以支持V2VBench数据集的批量视频编辑

---
+++ batch_edit_rf_solver.sh
@@ -0,0 +1,13 @@
+python edit_video.py \
+    --video-size 512 512 \
+    --video-length 25 \
+    --infer-steps 30 \
+    --inject 5 \
+    --seed 42 \
+    --embedded-cfg-scale 2 \
+    --flow-shift 7.0 \
+    --flow-reverse \
+    --use-cpu-offload \
+    --use-fp8 \
+    --save-path ./results/V2VBench/rf-solver_2order \
+    --dataset "V2VBench"
